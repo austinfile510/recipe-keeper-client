53275c2cabe99ec90e209ce1cb8be7e577820b09
Austin File <austinfile510@gmail.com>
2021-01-25 11:43:09 -0800

    added search bar to MyRecipes

--- src/MyRecipes/MyRecipes.js
+++ src/MyRecipes/MyRecipes.js
@@ -8,6 +8,14 @@ class MyRecipes extends React.Component {
 			<div>
 				<Nav />
 				<main role='main'>
+				<form>
+					<label>
+							Search for a recipe:{' '}
+							<input type='text' name='search' />
+							<input type='submit' value='Search' />
+						</label>
+						<br />
+					</form>
 					<RecipesList />
 				</main>
 			</div>
