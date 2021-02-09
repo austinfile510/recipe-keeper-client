6929c6d1eddb1b2937e83f22bdd025192ee47742
Austin File <austinfile510@gmail.com>
2021-02-08 20:03:12 -0800

    updated config.js, again

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-	API_ENDPOINT: `https://stormy-castle-23160.herokuapp.com/api`,
+	API_ENDPOINT: `https://stormy-castle-23160.herokuapp.com`,
 	TOKEN_KEY: 'recipe-keeper-auth-token',
 }
