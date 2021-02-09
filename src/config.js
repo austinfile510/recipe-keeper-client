4488fba6bd1f54aa26162473bbbd1f1fb0a39d06
Austin File <austinfile510@gmail.com>
2021-02-09 12:06:15 -0800

    edited config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-export default {
+const config = {
 	API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || `http://localhost:8000/api`,
 	TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'recipe-keeper-auth-token',
 }
+
+export default config;
