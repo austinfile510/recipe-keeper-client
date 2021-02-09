2bab165837de131479df9e36e19d694664b17f9c
Austin File <austinfile510@gmail.com>
2021-02-08 22:33:38 -0800

    updated config.js with new API_endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 export default {
-	API_ENDPOINT: `https://stormy-castle-23160.herokuapp.com`,
+	API_ENDPOINT: `https://pure-tundra-43062.herokuapp.com/api`,
 	TOKEN_KEY: 'recipe-keeper-auth-token',
+	DATABASE_URL:
+	process.env.DATABASE_URL || 'postgresql://postgres:wetbeverage@localhost/noteful',
+	TEST_DATABASE_URL:
+		process.env.DATABASE_URL ||
+		'postgresql://postgres:wetbeverage@localhost/noteful-test',
 }
